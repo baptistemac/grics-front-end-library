a4ea7f5b4284765d885798a16586d12baab8e661
Baptiste <baptiste.mac@gmail.com>
2021-06-03 00:07:46 -0400

    init Button and Card

---
+++ lib/src/components/Button/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <button type='button' class='btn' :class="classes">
+        {{ label }}
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'g-button',
+        inheritAttrs: false,
+        props: {
+            label       : String,
+            hierarchy   : String,
+            size        : String,
+        },
+        data() {
+            return {
+                classes: {
+                    'btn--primary'  : (this.hierarchy !== 'secondary'),
+                    'btn--secondary': (this.hierarchy === 'secondary'),
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .btn {
+        border: none;
+        padding: .5rem 1rem;
+        border-radius: 4px;
+        background-color: navajowhite;
+        cursor: pointer;
+    }
+
+    .btn--secondary {
+        border: 1px solid navajowhite;
+        background-color: transparent;
+    }
+</style>
---
+++ lib/src/components/Card/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <article class='card'>
+        <h3>{{ title }}</h3>
+        <g-button v-bind="$props" :label="buttonLabel" />
+    </article>
+</template>
+
+<script>
+    import GButton from '../Button';
+
+    export default {
+        name: 'g-card',
+        components: { GButton },
+        props: {
+            title       : String,
+            buttonLabel : String,
+        }
+    }
+</script>
+
+<style scoped>
+    .card {
+        border: 1px solid navajowhite;
+        padding: 1rem;
+    }
+</style>
